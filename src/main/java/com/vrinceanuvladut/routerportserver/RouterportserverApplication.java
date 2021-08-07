b923d039c72e5bc725fa82de25b8986cd8fb55ec
Wladut <vrinceanu.ioan.vladut@gmail.com>
2021-08-07 08:05:47 +0300

    LD-23 #comment create project first commit

---
+++ src/main/java/com/vrinceanuvladut/routerportserver/RouterportserverApplication.java
@@ -0,0 +1,13 @@
+package com.vrinceanuvladut.routerportserver;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RouterportserverApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(RouterportserverApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/vrinceanuvladut/routerportserver/RouterportserverApplicationTests.java
@@ -0,0 +1,13 @@
+package com.vrinceanuvladut.routerportserver;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RouterportserverApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
