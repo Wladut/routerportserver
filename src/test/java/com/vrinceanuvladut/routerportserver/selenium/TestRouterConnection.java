66d2b73566412511ded19077378a783e6fdf4f8f
Wladut <vrinceanu.ioan.vladut@gmail.com>
2021-08-18 23:14:31 +0300

    LD-23 #comment run test case

--- src/test/java/com/vrinceanuvladut/routerportserver/selenium/TestRouterConnection.java
+++ src/test/java/com/vrinceanuvladut/routerportserver/selenium/TestRouterConnection.java
@@ -8,21 +8,21 @@ class TestRouterConnection {
 
     @Test
     void loginPageTestPositiveCase(){
-        // Scanner scanner = new Scanner(System.in);
-        // System.out.println("Enter ip address (eg. https://192.168.1.1): ");
-        // String ip = scanner.nextLine();
-        // System.out.println("Enter username: ");
-        // String username = scanner.nextLine();
-        // System.out.println("Enter password: ");
-        // String password = scanner.nextLine();
+        Scanner scanner = new Scanner(System.in);
+        System.out.println("Enter ip address (eg. https://192.168.1.1): ");
+        String ip = scanner.nextLine();
+        System.out.println("Enter username: ");
+        String username = scanner.nextLine();
+        System.out.println("Enter password: ");
+        String password = scanner.nextLine();
 
-        // RouterConnection routerConnection = new RouterConnection("D:/sleniumdriver/chromedriver_win32/chromedriver.exe");
-        // routerConnection.loginPage(ip,username, password);
-        // routerConnection.openAccessControlPage();
-        // routerConnection.openPortForwardingPage();
-        // routerConnection.portForwardingStatus(true);
-        // routerConnection.logout();
-        // routerConnection.close();
+        RouterConnection routerConnection = new RouterConnection("D:/sleniumdriver/chromedriver_win32/chromedriver.exe");
+        routerConnection.loginPage(ip,username, password);
+        routerConnection.openAccessControlPage();
+        routerConnection.openPortForwardingPage();
+        routerConnection.portForwardingStatus(true);
+        routerConnection.logout();
+        routerConnection.close();
     }
     
 }
